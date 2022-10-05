7f7b0442feb40aa8e1ec10c7f0573c807c2852d4
lauren-cardenas <lauren.cardenas@sfdr-cisd.org>
2022-10-05 18:26:00 -0500

    Added things to Robot.Java that change the Limelight Pipeline

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -29,6 +29,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    Limelight.setPipeline(3);
     
   }
 
@@ -46,6 +47,8 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    Limelight.setPipeline(3);
+    Limelight.update();
     
   }
 
